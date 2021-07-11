edc4c355f34d3350c5495a02b498e2f8246c56f8
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2021-07-12 01:48:02 +0900

    ItemMetaが適応されない不具合を修正

--- src/main/java/com/github/shur/whitebait/inventory/Icon.kt
+++ src/main/java/com/github/shur/whitebait/inventory/Icon.kt
@@ -16,23 +16,23 @@ class Icon {
     var flags: MutableSet<ItemFlag> = mutableSetOf()
     var enchantments: MutableMap<Enchantment, Int> = mutableMapOf()
     var model: Int? = null
+
     // basedItemは元となるアイテムであり、editItemはパラーメーター適応後に処理される
-    var editItem: (ItemStack) -> Unit = {  }
+    var editItem: (ItemStack) -> Unit = { }
 
 
     fun toItemStack(): ItemStack {
         val itemStack = basedItem ?: ItemStack(type)
 
         itemStack.amount = amount
-        if (itemStack.hasItemMeta()) {
-            itemStack.itemMeta = itemStack.itemMeta!!.also { meta ->
-                meta.setDisplayName(name)
-                meta.lore = lore
-                meta.addItemFlags(*flags.toTypedArray())
-                enchantments.forEach { (enchant, level) -> meta.addEnchant(enchant, level, true) }
-                meta.setCustomModelData(model)
-            }
+        itemStack.itemMeta = itemStack.itemMeta?.also { meta ->
+            meta.setDisplayName(name)
+            meta.lore = lore
+            meta.addItemFlags(*flags.toTypedArray())
+            enchantments.forEach { (enchant, level) -> meta.addEnchant(enchant, level, true) }
+            meta.setCustomModelData(model)
         }
+
         itemStack.apply(editItem)
 
         return itemStack
